494e2e42c2edc61c3a939a8c833167dec2e3cc95
abijij <105397465+abijij@users.noreply.github.com>
2024-02-01 15:55:58 -0600

    Comunicacion entre microservicios exitosa

---
+++ src/main/java/com/abijij/springboot/app/items/springbootsercieitems/controller/ItemController.java
@@ -0,0 +1,28 @@
+package com.abijij.springboot.app.items.springbootsercieitems.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.abijij.springboot.app.items.springbootsercieitems.models.Item;
+import com.abijij.springboot.app.items.springbootsercieitems.models.service.ItemService;
+
+@RestController
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping("/toList")
+    public List<Item> toList() {
+        return itemService.findAll();
+    }
+
+    @GetMapping("/detail/{id}/cantidad/{cantidad}")
+    public Item detail(@PathVariable Long id, @PathVariable Integer cantidad) {
+        return itemService.findById(id, cantidad);
+    }
+
+}
--- src/main/java/com/abijij/springboot/app/items/springbootsercieitems/models/service/ItemServiceImpl.java
+++ src/main/java/com/abijij/springboot/app/items/springbootsercieitems/models/service/ItemServiceImpl.java
@@ -1,24 +1,39 @@
 package com.abijij.springboot.app.items.springbootsercieitems.models.service;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 
 import com.abijij.springboot.app.items.springbootsercieitems.models.Item;
+import com.abijij.springboot.app.items.springbootsercieitems.models.Product;
 
 @Service
 public class ItemServiceImpl implements ItemService{
 
-    
+    @Autowired
+    private RestTemplate clientRest;
+     
 
     @Override
     public List<Item> findAll() {
-        return null;
+        List<Product> products =Arrays.asList( clientRest.getForObject("http://localhost:8001/toList",Product[].class));
+        return products.stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
     }
 
     @Override
     public Item findById(Long id, Integer cantidad) {
-        return null;
+        Map<String, String> pathVariables = new HashMap<String, String>();
+        pathVariables.put("id", id.toString());
+        Product product = clientRest.getForObject("http://localhost:8001/detal/{id}", Product.class, pathVariables);
+
+        return new Item(product, cantidad);
     }
 
     
